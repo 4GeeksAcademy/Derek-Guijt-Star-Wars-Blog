b80ff8a445bc1bec489e5cbcb184580f74e99cbb
Derek Guijt <137216062+DerekGuijt@users.noreply.github.com>
2024-01-31 01:28:59 +0000

    first commit

--- src/js/views/PlanetDetail.js
+++ src/js/views/PlanetDetail.js
@@ -4,34 +4,29 @@ import { Context } from "../store/appContext";
 
 function PlanetDetail() {
   const { store, actions } = useContext(Context);
-  const [planet, setplanet] = useState({})
   const { id } = useParams();
+
 
   useEffect(() => {
     actions.fetchPlanetDetail(id);
-    setplanet(store.currentPlanets)
   }, []);
-
-  if (!store.currentPlanets) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <div className="card mt-5" style={{ width: "30rem", margin: "auto" }}>
-      <img
-        src={`https://starwars-visualguide.com/assets/img/planets/${parseInt(id) + 1}.jpg`}
+      {true? (<><img
+        src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
         className="card-img-top"
         alt={name}
         style={{ height: "30rem" }}
       />
       <div className="card-body">
-        <h1 className="card-title">{planet.name}</h1>
-        <p className="card-text">Height: {planet.height}</p>
-        <p className="card-text">Mass: {planet.mass}</p>
-        <p className="card-text">Hair Color: {planet.hair_color}</p>
-        <p className="card-text">Skin Color: {planet.skin_color}</p>
-        <p className="card-text">Eye Color: {planet.eye_color}</p>
-      </div>
+        <h1 className="card-title">{store.currentPlanet.name}</h1>
+        <p className="card-text">Height: {store.currentPlanet.height}</p>
+        <p className="card-text">Mass: {store.currentPlanet.mass}</p>
+        <p className="card-text">Hair Color: {store.currentPlanet.hair_color}</p>
+        <p className="card-text">Skin Color: {store.currentPlanet.skin_color}</p>
+        <p className="card-text">Eye Color: {store.currentPlanet.eye_color}</p>
+      </div></>) : (<div>Loading...</div>)}
     </div>
   );
 }
